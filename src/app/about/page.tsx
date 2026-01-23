2b58bf2863f1b70ed4badce227654e9341853441
ttcode10 <ttcode10@gmail.com>
2026-01-24 09:56:48 +1100

    about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import HeroSection from '@/components/HeroSection';
+import AboutSection from '@/components/AboutSection';
+import FeaturesSection from '@/components/FeaturesSection';
+import Brands from '@/components/BrandsSection';
+
+export default function Home() {
+  return (
+    <>
+      <AboutSection />
+      <FeaturesSection />
+      <Brands />
+    </>
+  );
+}
