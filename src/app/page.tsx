8e73b797da7dd4b4af9bc1820f1bc76d9a929941
ttcode10 <ttcode10@gmail.com>
2026-01-24 09:57:09 +1100

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import HeroSection from '@/components/HeroSection';
 import AboutSection from '@/components/AboutSection';
 import FeaturesSection from '@/components/FeaturesSection';
 import ProductsSection from '@/components/ProductsSection';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
 import ContactSection from '@/components/ContactSection';
 import Brands from '@/components/BrandsSection';
 import Partners from '@/components/PartnersSection';
